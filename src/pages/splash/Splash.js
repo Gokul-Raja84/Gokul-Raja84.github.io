915be4729a40b352a7056e283221cb8878279485
Gokul-Raja84 <gokulraja840@gmail.com>
2024-07-27 06:30:35 +0530

    Portfolio

--- src/pages/splash/Splash.js
+++ src/pages/splash/Splash.js
@@ -9,7 +9,7 @@ function AnimatedSplash(props) {
         <div className="preloader">
           <div className="texts-container">
             <span>Developer,</span>
-            <span>Porfolio,</span>
+            <span>Portfolio,</span>
             <span>Vibes.</span>
           </div>
         </div>
